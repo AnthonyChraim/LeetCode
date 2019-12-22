90aa44b1a54355f07e4db3ca18ba42730f585e82
TonyMontana11 <anth.chraim@gmail.com>
2019-12-21 21:29:00 -0500

    final

---
+++ SumAndProd/src/sumAndProd.java
@@ -0,0 +1,28 @@
+
+
+//Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+
+public class sumAndProd {
+
+	public static void main(String[] args) {
+
+		System.out.println(substractProductAndSum(4421));
+		
+	}
+
+	public static int substractProductAndSum(int n) {
+			int product = 1;
+			int sum = 0;
+			
+			for(int i = 0; i < 5; i++) {
+				if( n == 0)
+					break;
+				product *= n % 10 ;
+				sum += n % 10;
+				n /= 10;	
+			}
+			
+			return product - sum;
+	}
+}
