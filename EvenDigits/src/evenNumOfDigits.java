d05532ad0867e292e93d0ca214c620eb2e4eadb9
TonyMontana11 <anth.chraim@gmail.com>
2019-12-23 15:29:00 -0500

    beats 98%, 100%

--- EvenDigits/src/evenNumOfDigits.java
+++ EvenDigits/src/evenNumOfDigits.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 //Given an array nums of integers, return how many of them contain an even number of digits
 
 public class evenNumOfDigits {
@@ -5,7 +7,7 @@ public class evenNumOfDigits {
 	public static void main(String[] args) {
 		
 		int[] nums = {12, 345, 2, 6, 7896};
-		System.out.println(findNumbers(nums));
+		System.out.println(findNumbers3(nums));
 //
 	}
 	
@@ -18,6 +20,29 @@ public class evenNumOfDigits {
 		}
 		return finalNum;
 		
+	}
+	
+	//one liner
+	public static int findNumbers2 (int[] nums) {
+		return (int) Arrays.stream(nums)
+				.filter(x -> String.valueOf(x).length() % 2 == 0)
+				.count();
+	}
+	
+	public static int findNumbers3 (int[] nums) {
+		int finalNum = 0;
+		
+		for (int num : nums) {
+			int count = 0;
+			while(num != 0) {
+				count ++;
+				num /= 10;
+			}
+			if(count % 2 == 0)
+				finalNum ++;
+		}
+		
+		return finalNum;
 	}
 	
 }
